338f308bc17fb369b82d85ec12231b31152beba4
John Diego <jadiego@uw.edu>
2017-03-29 17:56:29 -0700

    In class work

--- zipsvr/main.go
+++ zipsvr/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path"
+	"strings"
 )
+
+type zip struct {
+	Zip   string `json:"zip"`
+	City  string `json:"city"`
+	State string `json:"state"`
+}
+
+type zipSlice []*zip
+type zipIndex map[string]zipSlice
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
@@ -15,6 +27,22 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello " + name))
 }
 
+//* means receving a pointer
+func (zi zipIndex) zipsForCityHandler(w http.ResponseWriter, r *http.Request) {
+	// /zips/city/seattle
+	_, city := path.Split(r.URL.Path)
+	lcity := strings.ToLower(city)
+
+	w.Header().Add("Cotent-Type", "application/json; charset=utf-8")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+
+	encoder := json.NewEncoder(w)
+	if err := encoder.Encode(zi[lcity]); err != nil {
+		http.Error(w, "error encoding json: "+err.Error(), http.StatusInternalServerError)
+	}
+
+}
+
 func main() {
 	addr := os.Getenv("ADDR")
 	fmt.Println(addr)
@@ -22,7 +50,33 @@ func main() {
 		log.Fatal("please set ADDR environment variable")
 	}
 
+	f, err := os.Open("../data/zips.json")
+	if err != nil {
+		log.Fatal("error opening zips file: " + err.Error())
+	}
+
+	zips := make(zipSlice, 0, 43000)
+	decoder := json.NewDecoder(f) //reads strram of bytes
+	//if there is an error then handle error
+	if err := decoder.Decode(&zips); err != nil {
+		log.Fatal("error decoding zips json: " + err.Error())
+	}
+	fmt.Printf("loaded %d zips\n", len(zips))
+
+	zi := make(zipIndex)
+
+	// range is like foreach loops
+	// iterating for pointers to zips
+	for _, z := range zips {
+		lower := strings.ToLower(z.City)
+		zi[lower] = append(zi[lower], z)
+	}
+
+	fmt.Printf("there are %d zips in Seattle\n", len(zi["seattle"]))
+
 	http.HandleFunc("/hello", helloHandler)
+
+	http.HandleFunc("/zips/city/", zi.zipsForCityHandler)
 
 	fmt.Printf("server is listening at %s...\n", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
